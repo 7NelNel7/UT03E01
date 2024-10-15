1c14b32b2c148124c5643000e824703564d4270b
7NelNel7 <daniel.tejtej.1@educa.jcyl.es>
2024-10-15 12:06:47 +0200

    retoques

--- UT03E01.js
+++ UT03E01.js
@@ -1,27 +1,27 @@
 function repetidos(arr) {
-    // Crear un objeto para almacenar la cuenta de cada número
+    
     const conteo = {};
 
-    // Contar la frecuencia de cada elemento
+  
     for (const num of arr) {
         conteo[num] = (conteo[num] || 0) + 1;
     }
 
-    // Generar el array de salida con las repeticiones en el orden de aparición
+    
     const resultado = [];
-    const numerosProcesados = new Set(); // Usamos un conjunto para llevar la cuenta de los números que ya procesamos
+    const numerosProcesados = new Set(); 
 
     for (const num of arr) {
-        if (!numerosProcesados.has(num)) { // Verificamos si el número ya fue procesado
+        if (!numerosProcesados.has(num)) { 
             resultado.push(conteo[num]);
-            numerosProcesados.add(num); // Lo marcamos como procesado
+            numerosProcesados.add(num); 
         }
     }
 
     return resultado;
 }
 
-// Ejemplos de uso
-console.log(repetidos([2, 3, 2, 2])); // [3, 1]
-console.log(repetidos([1, 1, 1]));    // [3]
-console.log(repetidos([2, 3, 3, 2])); // [2, 2]
+
+console.log(repetidos([2, 3, 2, 2])); 
+console.log(repetidos([1, 1, 1]));    
+console.log(repetidos([2, 3, 3, 2])); 
